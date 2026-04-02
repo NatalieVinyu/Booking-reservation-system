d9ce4e26cf9fcf481ca2750391bb3cf74e309d62
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-04-02 19:08:43 +0200

    added components and files

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+//MAIN BOOKING LOGIC & UI FLOW
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='p-6 max-w-xl mx-auto bg-white shadow rounded'>
+      <h1 className='text-2xl font-bold mb-4'>Vet Appointment Booking</h1>
+    </div>
+  )
+}
+
+export default Home
